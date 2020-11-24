54ed79133c424b7a8ef419726826d9c7a8ab599d
reivak720 <74047576+reivak720@users.noreply.github.com>
2020-11-24 21:06:18 +0000

    docstrings

--- kingsTour.py
+++ kingsTour.py
@@ -9,10 +9,16 @@ from operator import itemgetter
 
 sys.setrecursionlimit(1500)
 class BoardGraph(object):
-    '''Square room graph, connecting continous tile spaces'''
-
+    '''
+    Square room graph, connecting continous tile spaces
+    '''
     def __init__(self, height, width): 
-        '''creates a graph for a square room of dimension height x width''' 
+        '''
+        Parameters:
+        height: type int
+        width: type int
+        creates a graph for a square room of dimension height x width
+        ''' 
         self.width = width
         self.height = height  
         self.nodes =  list(product(range(height), range(width)))
@@ -20,6 +26,9 @@ class BoardGraph(object):
         self.breed()
 
     def breed(self):
+        '''
+        Connects nodes and children
+        '''
         for node in self.nodes:
             for children in self.nodes:
                 if children != node and abs(node[0] - children[0]) <=1 and abs(node[1] - children[1]) <=1:
@@ -27,16 +36,26 @@ class BoardGraph(object):
 
 
     def cornerWeight(self, child, corner):
+        '''
+        the closer the coordinate is to the corner, lower value
+        '''
         value = abs(child[0]-corner[0]) + abs(child[1] - corner[1])
         return value
     
     def getCorner(self, node):
+        '''
+        get the closest corner to a node
+        '''
         corners = [(0,0), (0, self.width - 1), (self.height-1, 0), (self.height-1, self.width-1)]
         for corner in corners:
             if abs(corner[0] - node[0]) < self.height/2 and abs(corner[1] - node[1]) < self.width/2:
                 return corner 
         
     def updateWeight(self, node, path):
+        '''
+        update weights for children of a node based on their 
+        available children and distance to corner
+        '''
         corner = self.getCorner(node)
         for children in self.childrenOf(node):
             available_children = 0
@@ -47,14 +66,28 @@ class BoardGraph(object):
             self.edges[node][children] = available_children 
 
     def childrenOf(self, node):
-       
-        children = sorted(self.edges[node].items(), key = itemgetter(1))
-        return[child[0] for child in children]
+       '''
+       returns the children of a node
+       '''
+       children = sorted(self.edges[node].items(), key = itemgetter(1))
+       return[child[0] for child in children]
 
+    def getNodes(self):
+        '''
+        returns nodes in graph
+        '''
+        return self.nodes
+    
     def getEdges(self):
-        return self.edges.keys()
+        '''
+        returns self.edges
+        '''
+        return self.edges
 
     def printGraph(self):
+        '''
+        prints nodes and children
+        '''
         for node, children in self.edges.items():
             print ("Node:", node, "     Children:", children)
 
@@ -62,27 +95,47 @@ class BoardGraph(object):
 class PathFinder(object):
 
     def __init__(self, height, width, start):
+        '''
+        Parameters:
+        height = type int
+        width = type int
+        start = type tuple (y<height, x<width)
+        gets path between a starting point within a graph
+        passing by all nodes
+        '''
         self.height = height
         self.width = width
         self.graph = BoardGraph(height, width)
-        if start not in self.graph.getEdges():
+        if start not in self.graph.getNodes():
             raise ValueError ('Start possition must be within graph.')
         self.start = start
         self.path = self.goDeep(start)
 
     def get_start(self):
+        '''
+        returns starting point
+        '''
         return self.start
     
     def get_graph(self):
+        '''
+        returns graph
+        '''
         return self.graph
     
     def get_path(self):
+        '''
+        returns path
+        '''
         return self.path
 
     def goDeep(self, start, path=[]): 
+        '''
+        recursive deapht-first search
+        '''
         node = start
         path.append(node)   
-        if all(node in path for node in self.graph.getEdges()):
+        if all(node in path for node in self.graph.getNodes()):
             return path    
         self.graph.updateWeight(node, path)
         for n in self.graph.childrenOf(node):
@@ -95,6 +148,11 @@ class PathFinder(object):
 
 
     def representSolution(self):
+        '''
+        represents path as an array
+        1 is starting point and largest number
+        is end point
+        '''
         model = np.zeros((self.height, self.width), dtype = int)
         step = 1
         for coordinate in self.path:
